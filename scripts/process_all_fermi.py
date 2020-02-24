eb32bcd3c2df9050b0b40c4577064ec4a87fb53a
Travis <ymon0003@Traviss-MacBook-Pro.local>
2020-02-24 23:18:24 +1100

    record those procesed Fermi events

--- scripts/process_all_fermi.py
+++ scripts/process_all_fermi.py
@@ -14,11 +14,17 @@ fermi_id = [s.split('_T')[0] for s in fermi_df['target'].values]
 fermi_id = list(set(fermi_id))
 fermi_id.sort(reverse=True)
 
+with open("processed_id.txt") as f:
+    processed_event = [s.split('\n')[0] for s in f.readlines()]
+
 for fid in fermi_id:
-    if not os.path.isdir('./{}'.format(fid)):
-        os.mkdir('./{}'.format(fid))
-    os.chdir('./{}'.format(fid))
-    gf.GenerateReports.TemplateComparing(fid, day=1, subtract=False, score=0.5, near_galaxy=True)
-    os.system("rm -rf *.fz")
-    os.chdir('../')
+    if not fid in processed_event:
+        if not os.path.isdir('./{}'.format(fid)):
+            os.mkdir('./{}'.format(fid))
+        os.chdir('./{}'.format(fid))
+        gf.GenerateReports.TemplateComparing(fid, day=1, subtract=False, score=0.5, near_galaxy=True)
+        os.system("rm -rf *.fz")
+        os.chdir('../')
+        with open("processed_id.txt", 'a+') as f:
+            f.writelines('{}\n'.format(fid)) 
 
