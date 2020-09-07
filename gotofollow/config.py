3e15d139a97b43aae922c2cd3eb29ef4c54636da
ewittmyl <yik.mong@monash.edu>
2020-09-07 11:56:40 +1000

    debug

--- gotofollow/config.py
+++ gotofollow/config.py
@@ -3,4 +3,4 @@ import pkg_resources
 
 DATA_PATHS = '/export/'
 IMG_PATHS = ['/export/' + s + '/gotophoto/storage/pipeline/' for s in os.listdir('/export/')]
-IMG_PATHS = [s for s in os.listdir(s) if os.path.exists(s)]
+IMG_PATHS = [s for s in os.listdir(IMG_PATHS) if os.path.exists(s)]
